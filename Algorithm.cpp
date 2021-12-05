723802aee9c5dd7ab9464e9f5e0713f295932132
MalininArt <malininart@mail.ru>
2021-12-05 13:19:15 +0300

    Update Algorithm.cpp

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -4,24 +4,29 @@
 using namespace std;
 static int g_n;
 
-void SubsetBySet(string str, int len, int* _set)
+void SubsetBySet(string str, int len, int *_set, int *SeqNumber)
 {
     if (!len) {
+        cout << *SeqNumber << "->" << "{";
         for (int i = 0; i < g_n; i++)
             if (str[i] == '1')
                 cout << _set[i] << " ";
-        cout << endl;
+        cout <<"}"<< endl;
+        (*SeqNumber)++;
         return;
     }
-    SubsetBySet(str + '0', len - 1, _set);
-    SubsetBySet(str + '1', len - 1, _set);
+    SubsetBySet(str + '0', len - 1, _set,SeqNumber);
+    SubsetBySet(str + '1', len - 1, _set,SeqNumber);
 }
 
 int main()
 {
-    int* _set;
-    int* _set3;
+    int *_set;
+    int *_set3;
+    int *SeqNumber;
     g_n = 3;
+    SeqNumber = new int(1);
+    
 
     _set3 = new int[g_n];
     cout << "Set for 3" << endl;
@@ -31,14 +36,15 @@ int main()
     }
     cout << endl;
 
-    cout << "Subset";
-    SubsetBySet("", g_n, _set3);
+    cout << "Subset"<< endl;
+    SubsetBySet("", g_n, _set3,SeqNumber);
 
     cout << "O(n)"<< endl;
 
 
     cout << "n = ";
     cin >> g_n;
+    *SeqNumber = 1;
 
     _set = new int[g_n];
     cout << "Set" << endl;
@@ -48,10 +54,11 @@ int main()
     }
     cout << endl;
 
-    cout << "Subset";
-    SubsetBySet("", g_n, _set);
+    cout << "Subset"<< endl;
+    SubsetBySet("", g_n, _set,SeqNumber);
     delete[] _set;
     delete[] _set3;
+    delete(SeqNumber);
 
     return 0;
 }
